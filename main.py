cc730b465f9509e17be5862d293b01eb8cf1835a
Florian <florianmarkiewicz1@gmail.com>
2022-03-31 16:48:59 +0200

    zrobione wyszukiwanie przez dzien

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+from flask import Flask, jsonify, render_template, request, session
+
+app = Flask(__name__)
 
 response = requests.get("https://wedkuje.pl/kalendarz-wedkarski-bran-ryb.html")
 website = response.text
@@ -19,4 +22,21 @@ result = {}
 for i in range(len(days)):
     result[days[i]] = [moon_phases[i], bites[i]]
 
-print(result)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/search")
+def get_day():
+    day = request.args.get("day")
+
+    return jsonify(day=day,
+                   moon_phase=result[day][0],
+                   bites=result[day][1],
+                   )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
