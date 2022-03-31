1a4a622607d188338297c815a573addbbd9d844c
Florian <florianmarkiewicz1@gmail.com>
2022-03-31 15:54:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+website = response.text
+soup = BeautifulSoup(website, "html.parser")
